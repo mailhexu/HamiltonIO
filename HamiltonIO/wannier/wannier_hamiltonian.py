632414ac6c275af64dfe984b02297e029707c5f2
mailhexu <mailhexu@gmail.com>
2026-03-19 15:47:24 +0100

    restore the change of /2 and +conj()

--- HamiltonIO/wannier/wannier_hamiltonian.py
+++ HamiltonIO/wannier/wannier_hamiltonian.py
@@ -211,7 +211,7 @@ class WannierHam(Hamiltonian):
             for iR, (R, mat) in enumerate(self.data.items()):
                 phase = np.exp(self.R2kfactor * np.dot(k, R))  # / self.R_degens[iR]
                 H = mat * phase
-                Hk += H
+                Hk += H + H.conjugate().T
         elif convention == 1:
             for iR, (R, mat) in enumerate(self.data.items()):
                 phase = (
@@ -219,7 +219,7 @@ class WannierHam(Hamiltonian):
                     # / self.R_degens[iR]
                 )
                 H = mat * phase
-                Hk += H
+                Hk += H + H.conjugate().T
         else:
             raise ValueError("convention should be either 1 or 2.")
         return Hk
